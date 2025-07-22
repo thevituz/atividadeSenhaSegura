1cd9095b4c3db8665a8762421476622f8d9d7b68
thevituz <yagovictorpaz@hotmail.com>
2025-07-22 11:33:41 -0300

    teste

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-$hostname = "localhost";
-$usuario = "root";
-$senha = "";
+$hostname = "sql301.infinityfree.com";
+$usuario = "if0_39533670";
+$senha = "ABfTOY8chEjx";
 $bancodedados = "banco_php";
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
